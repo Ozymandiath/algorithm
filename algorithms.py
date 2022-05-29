11e33ef283cc473e591df17f34c2ea058ac5ffe9
Ozymandiath <103983856+Ozymandiath@users.noreply.github.com>
2022-05-29 20:36:22 +0500

    Добавлено 3 метода сортировки.

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,4 @@
-def cyclic_shift(nums_left, nums_right=[]):
+def cyclic_shift(nums_left, nums_right=[1, 2, 3]):
     """
     Алгоритм циклический сдвиг.
     Сдвиг может происходить влево или вправо.
@@ -16,4 +16,35 @@ def cyclic_shift(nums_left, nums_right=[]):
     return nums_left
 
 
+def insert_sort(nums):
+    """
+    Сортировка списка nums вставками.
+    O(n^2).
+    """
+    for top in range(1, len(nums)):
+        k = top
+        while k > 0 and nums[k - 1] > nums[k]:
+            nums[k], nums[k - 1] = nums[k - 1], nums[k]
+            k -= 1
 
+
+def choise_sort(nums):
+    """
+    Сортировка списка nums выбором.
+    O(n^2).
+    """
+    for pos in range(0, len(nums) - 1):
+        for k in range(pos + 1, len(nums)):
+            if nums[k] < nums[pos]:
+                nums[k], nums[pos] = nums[pos], nums[k]
+
+
+def bubble_sort(nums):
+    """
+    Сортировка списка nums методом пузырька
+    O(n^2).
+    """
+    for bypass in range(1, len(nums)):
+        for k in range(0, len(nums) - bypass):
+            if nums[k] > nums[k + 1]:
+                nums[k], nums[k + 1] = nums[k + 1], nums[k]
